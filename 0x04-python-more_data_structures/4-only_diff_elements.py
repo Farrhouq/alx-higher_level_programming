4f4f83358f142f5da153cb15d584c81d9bf478b9
Farrhouq <farouqimoro@gmail.com>
2024-01-24 14:00:00 +0000

    update

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    a_only = {ele for ele in set_1 if not ele in set_2}
-    b_only = {ele for ele in set_2 if not ele in set_1}
+    a_only = {ele for ele in set_1 if ele not in set_2}
+    b_only = {ele for ele in set_2 if ele not in set_1}
     for i in b_only:
         a_only.add(i)
     return a_only
