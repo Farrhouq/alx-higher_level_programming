4471ce1283974440deb2e9846ac3646b68e84946
Farrhouq <farouqimoro@gmail.com>
2024-02-04 05:14:13 +0000

    update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,10 +5,8 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[i], end='')
             count += 1
-        except IndexError:
+        except (IndexError, ValueError):
             print()
-            return count
-        finally:
             return count
     print()
     return count
