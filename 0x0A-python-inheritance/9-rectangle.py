535d4971d6219745859630ae72ad31d3a121b935
Farrhouq <farouqimoro@gmail.com>
2024-02-18 19:20:13 +0000

    update

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Contains a Rectangle class"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Implements a Rectangle class"""
+
+    def __init__(self, width, height):
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """implements the area"""
+        return self.__width + self.__height
+
+    def __str__(self):
+        return f"[Rectange] {self.__width}/{self.__height}"
