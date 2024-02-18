22d464d696f1d04f1a85cf1e23b44b1821e9042d
Farrhouq <farouqimoro@gmail.com>
2024-02-18 17:29:21 +0000

    update

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Contains function that checks a class against some"""
+
+
+def is_same_class(obj, a_class):
+    """Checks if obj is an instance of a_class"""
+    return obj.is_instance(a_class)
