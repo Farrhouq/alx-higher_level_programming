8a11b911b74d18e17b2f974e3b4966fe18ee1491
Farrhouq <farouqimoro@gmail.com>
2024-02-18 17:27:37 +0000

    update

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """This module contains a class MyList that inheritsf from 'list'"""
 
+
 class MyList(list):
     """Custom list inheriting from python list"""
+
     def print_sorted(self):
-        l = self.copy()
-        l.sort()
-        print(l)
-        del l
+        list_copy = self.copy()
+        list_copy.sort()
+        print(list_copy)
+        del list_copy
