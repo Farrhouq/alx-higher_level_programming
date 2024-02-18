05d2341daeaa2508088ad1884279a1c61e7d8228
Farrhouq <farouqimoro@gmail.com>
2024-02-18 23:49:03 +0000

    update

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Contains a Square class"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Implements a Rectangle class"""
+
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """calculates the area of the square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
