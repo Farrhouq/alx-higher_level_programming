0690bf074bbcdc4a47be0144efc0cb8948b357fd
Farrhouq <farouqimoro@gmail.com>
2024-02-19 00:00:07 +0000

    update

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -3,7 +3,7 @@
 if it's possible"""
 
 
-def add_attribute(obj:any, attr, value):
+def add_attribute(obj, attr, value):
     """adds a new attribute to an object if it's possible"""
     if hasattr(obj, '__dict__'):
         setattr(obj, attr, value)
