c77f00510bfe6091b8f6a601f910ec0cfaad0218
Farrhouq <farouqimoro@gmail.com>
2024-02-19 11:40:17 +0000

    update

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Contains a Student class"""
+
+
+class Student:
+    """A Student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        return {k: self.__dict__[k] for k in self.__dict__ if k in attrs}
