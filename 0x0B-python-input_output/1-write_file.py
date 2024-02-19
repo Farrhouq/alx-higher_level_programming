7e6e8bd0edb2411cda32df918403cabc46bc81c0
Farrhouq <farouqimoro@gmail.com>
2024-02-19 10:17:48 +0000

    update

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,6 +2,7 @@
 """Contains a function that writes a string to a text file (UTF8)
 and returns the number of characters written"""
 
+
 def write_file(filename="", text=""):
     """ writes a string to a text file (UTF8) and returns the
     number of characters written"""
