e73ae76d6fa23fe5c2e17a51a94ef286b1a9d139
Farrhouq <farouqimoro@gmail.com>
2024-02-27 00:53:54 +0000

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -31,3 +31,10 @@ class Base:
         with open(filename, 'w') as file:
             file.write(cls.to_json_string([obj.to_dictionary()
                                            for obj in list_objs]))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list of dictionaries represented by json_string"""
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
