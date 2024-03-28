fc6fd336f14bbb8a3ce76cb09e76c3be2dc117e2
Farrhouq <farouqimoro@gmail.com>
2024-03-28 17:12:01 +0000

    update

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
--- Creates a table called first table
-CREATE TABLE first_table;
+-- Creates a table called first table if not exists
+CREATE TABLE IF NOT EXISTS first_table (id real, name varchar(256));
