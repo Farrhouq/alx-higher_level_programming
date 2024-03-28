c95fddc6b3e9b855da1e5721ddd866005c8e86b1
Farrhouq <farouqimoro@gmail.com>
2024-03-28 21:20:54 +0000

    update

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,8 +1,21 @@
 -- Convert the database to UTF-8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert the table to UTF-8
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- For each database:
+ALTER DATABASE
+    hbtn_0c_0
+    CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_unicode_ci;
 
--- Convert the field 'name' to UTF-8
-ALTER TABLE first_table MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- For each table:
+ALTER TABLE
+    first_table
+    CONVERT TO CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+
+-- For each column:
+ALTER TABLE
+    first_table
+    CHANGE name name
+    VARCHAR(256)
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
