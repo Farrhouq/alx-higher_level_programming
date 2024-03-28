79a7988ec128ad68d69a0c21859d6d01990ec4ad
Farrhouq <farouqimoro@gmail.com>
2024-03-28 20:59:56 +0000

    update

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,9 +1,8 @@
+-- Convert the database to UTF-8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert all tables within the database to UTF-8
-SELECT 
-    CONCAT('ALTER TABLE `', table_name, '` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;') AS stmt
-FROM 
-    information_schema.tables
-WHERE 
-    table_schema = 'hbtn_0c_0' AND table_type = 'BASE TABLE';
+-- Convert the table to UTF-8
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert the field 'name' to UTF-8
+ALTER TABLE first_table MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
