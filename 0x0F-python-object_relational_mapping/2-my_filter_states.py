3cb467c523d8e7c2055cbd1ba4183d463e776de8
Farrhouq <farouqimoro@gmail.com>
2024-05-09 17:45:14 +0000

    update

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module states with a name searched from the database hbtn_0e_0_usa"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    searched_name = sys.argv[4]
+    print(searched_name)
+
+    db = MySQLdb.connect(user=username, password=password,
+                         database=db_name, port=3306, host="localhost")
+    c = db.cursor()
+    c.execute("""SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC""".format(searched_name))
+    data = c.fetchall()
+
+    for row in data:
+        print(row)
+    db.close()
