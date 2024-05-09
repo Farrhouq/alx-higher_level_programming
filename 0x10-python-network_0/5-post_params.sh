662a2634850f64bd0ed76464a0b6b343c078fdbe
Farrhouq <farouqimoro@gmail.com>
2024-05-09 14:41:04 +0000

    update

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #11;rgb:0000/0000/0000!/bin/bash
 # This script sends a POST request
-curl -s -X POST -d 'email: test@gmail.com' -d 'subject: I will always be here for PLD' $1
+curl -s -d 'email=test@gmail.com&subject=I will always be here for PLD' $1
