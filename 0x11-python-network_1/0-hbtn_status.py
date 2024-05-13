013e17e7f21bc46a7136cec02c86d8d978c91697
Farrhouq <farouqimoro@gmail.com>
2024-05-13 23:09:40 +0000

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """This module checks your status"""
 
-import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    res = response.read()
-    print("Body response:")
-    print(f"\t- type: {type(res)}")
-    print(f"\t - content: {res}")
-    print(f"\t - utf8 content: {res.decode('utf-8')}")
+if __name__ == "__main__":
+    import urllib.request
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        res = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(res)}")
+        print(f"\t - content: {res}")
+        print(f"\t - utf8 content: {res.decode('utf-8')}")
