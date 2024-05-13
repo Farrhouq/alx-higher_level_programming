6116c0e9bfdeb6982e33143a6339de1af83f4e03
Farrhouq <farouqimoro@gmail.com>
2024-05-13 23:19:49 +0000

    update

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,9 +3,11 @@
 
 if __name__ == "__main__":
     import urllib.request
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        res = response.read()
-        print("Body response:")
-        print(f"\t- type: {type(res)}")
-        print(f"\t - content: {res}")
-        print(f"\t - utf8 content: {res.decode('utf-8')}")
+    import sys
+    
+
+    
+    url = sys.argv[1]
+
+    with urllib.request.urlopen(url) as response:
+        print(response.get_header('X-Request-Id'))
