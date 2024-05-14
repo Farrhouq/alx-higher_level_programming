b0885aa434a03a281a4853507bb2221594e80a06
Farrhouq <farouqimoro@gmail.com>
2024-05-14 10:51:39 +0000

    update

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This fetches all states with a in their name"""
+
+from sqlalchemy.orm import sessionmaker
+
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    inst = session.query(State).filter(State.name == sys.argv[4])
+    if not inst.first():
+        print("Nothing")
+    else:
+        print(f"{inst.first().id}")
