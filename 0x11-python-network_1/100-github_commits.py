d546ea56297b1927c546d30aab3d80c27f1de6e6
Farrhouq <farouqimoro@gmail.com>
2024-05-14 08:10:13 +0000

    update

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
             'X-GitHub-Api-Version': '2022-11-28',
             'Accept': 'application/vnd.github+json'})
     try:
-        for commit in res.json():
+        for commit in sorted(res.json(), key=lambda x: x['commit']['author']['date']):
             print(f"{commit['sha']}: {commit['commit']['author']['name']}")
     except Exception as e:
         print(e)
