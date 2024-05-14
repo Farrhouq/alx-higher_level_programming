30139ef42d3405fc07564334b71551331a9971bf
Farrhouq <farouqimoro@gmail.com>
2024-05-14 06:26:44 +0000

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
         res = response.read()
         print("Body response:")
         print(f"\t- type: {type(res)}")
-        print(f"\t - content: {res}")
-        print(f"\t - utf8 content: {res.decode('utf-8')}")
+        print(f"\t- content: {res}")
+        print(f"\t- utf8 content: {res.decode('utf-8')}")
